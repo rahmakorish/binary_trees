52bdad980a7025814d6f1c14a5c2172f9ad8c50e
rahma korish <rahmakorish@gmail.com>
2024-02-28 07:06:05 -0800

    inorder traverse

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ *binary_tree_inorder-goes through a binary tree using in-order traversal
+ *@tree: pointer to the root node of the tree to traverse
+ *@func: pointer to a function to call for each node
+ **/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	else if (tree != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			if (tree->left->left != NULL)
+				(*func)(tree->left->left->n);
+			(*func)(tree->left->n);
+			if (tree->left->right != NULL)
+				(*func)(tree->left->right->n);
+		}
+		if (tree->right != NULL)
+		{
+			if (tree->right->left != NULL)
+				(*func)(tree->right->left->n);
+			(*func)(tree->right->n);
+			if (tree->right->right != NULL)
+				(*func)(tree->right->right->n);
+		}
+	}
+}
--- 7-main.c
+++ 7-main.c
@@ -23,13 +23,13 @@ int main(void)
 
 	        root = binary_tree_node(NULL, 98);
 		    root->left = binary_tree_node(root, 12);
-		    root->right = binary_tree_node(root, 402);
+		        root->right = binary_tree_node(root, 402);
 			    root->left->left = binary_tree_node(root->left, 6);
-			        /*root->left->right = binary_tree_node(root->left, 56);*/
-				   /* root->right->left = binary_tree_node(root->right, 256);*/
-				        /*root->right->right = binary_tree_node(root->right, 512);*/
+			        root->left->right = binary_tree_node(root->left, 56);
+				    root->right->left = binary_tree_node(root->right, 256);
+				        root->right->right = binary_tree_node(root->right, 512);
 
 					    binary_tree_print(root);
-					        binary_tree_preorder(root, &print_num);
+					        binary_tree_inorder(root, &print_num);
 						    return (0);
 }
