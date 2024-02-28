34912d520525ae3bf0cd2d95df025a29b400c838
rahma korish <rahmakorish@gmail.com>
2024-02-28 08:14:23 -0800

    tree aize

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ *binary_tree_size- measures the size of a binary tree
+ *@tree:the root node of the tree to measure the height.
+ *Return:sizeof tree
+ **/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+	else if (tree != NULL)
+	{
+		size += 1;
+		if (tree->left)
+		{
+			size += 1;
+			if (tree->left->left)
+				size += 1;
+			if (tree->left->right)
+				size += 1;
+		}
+		if (tree->right)
+		{
+			size += 1;
+			if (tree->right->left)
+				size += 1;
+			if (tree->right->right)
+				size += 1;
+		}
+	}
+	return (size);
+}
