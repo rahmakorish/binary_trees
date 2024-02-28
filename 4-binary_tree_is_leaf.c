633ff3ca6279d39ddaedf367cf870ff47d910c61
rahma korish <rahmakorish@gmail.com>
2024-02-28 05:01:01 -0800

    is node a leaf?

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	else if(node->right == NULL && node->left == NULL)
+	else if (node->right == NULL && node->left == NULL)
 		return (1);
 	else
 		return (0);
