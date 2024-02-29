670f8292fdda84649db1dd6b2f34b4ac558b570e
rahma korish <rahmakorish@gmail.com>
2024-02-29 07:55:21 -0800

    balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_balance-measures the balance factor of a binary tree
+ * @tree:the root node of the tree to measure the balance factor
+ * Return:balance factor of BST
+ **/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int  lht = 0, rht = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree->left != NULL)
+	{
+		if (tree->left)
+		{
+			lht += 1;
+			tree = tree->left;
+		}
+	}
+	tree = tree;
+	while (tree->right != NULL)
+	{
+		if (tree->right)
+		{
+			rht += 1;
+			tree = tree->right;
+		}
+	}
+		return (lht - rht);
+}
