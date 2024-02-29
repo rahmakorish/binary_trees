cf6662211d408ebc6e9026bab61356e0d97a0f32
rahma korish <rahmakorish@gmail.com>
2024-02-29 06:52:31 -0800

    tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,12 +7,13 @@
  **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	 size_t depth = 0;
+	 size_t depth;
 
 	if (tree == NULL)
 		return (0);
 	else if (tree != NULL)
 	{
+		depth = 0;
 		if (tree->parent != NULL)
 		{
 			depth += 1;
