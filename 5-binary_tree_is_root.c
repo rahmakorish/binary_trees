00d8c07aa797a2254e1c44bf8986dc86eb803b32
rahma korish <rahmakorish@gmail.com>
2024-02-29 06:01:26 -0800

    is node a root?

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,8 +9,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	else if (node->right != NULL && node->left != NULL)
+	else if (node->right != NULL && node->left != NULL && node->parent == NULL)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
