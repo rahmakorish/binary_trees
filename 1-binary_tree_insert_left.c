b1278b0328b614f29712535fb337f68fba72a961
rahma korish <rahmakorish@gmail.com>
2024-02-29 05:54:39 -0800

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *lefty = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL || lefty == NULL)
+	if (lefty == NULL)
 		return (NULL);
+	if (parent == NULL)
+	{
+		free(lefty);
+		return (NULL);
+	}
 	else if (lefty != NULL && parent != NULL)
 	{
 		lefty->n = value;
